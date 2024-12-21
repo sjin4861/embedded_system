0ec4667d1a4434d70f1f8410a365f8c1562afdce
sjin4861 <sjin4861@pusan.ac.kr>
2024-12-21 10:42:21 +0900

    feat : 초음파 센서 하나 성공

--- project_modules/main.c
+++ project_modules/main.c
@@ -44,12 +44,12 @@
    신호등 2 RED   : PB8
    ----------------------------------------------------------------
    블루투스 모듈 핀 매핑 (USART1 사용)
-   TX 핀: PA9 (USART1_RX)
-   RX 핀: PA10 (USART1_TX)
+   TX 핀: PA9 (USART1_TX)
+   RX 핀: PA10 (USART1_RX)
 
    (USART2 사용)
-   TX 핀: PD5 (USART1_RX)
-   RX 핀: PD6 (USART1_TX)
+   TX 핀: PD5 (USART2_TX)
+   RX 핀: PD6 (USART2_RX)
    ----------------------------------------------------------------
    스텝모터 모듈 핀 매핑
 
@@ -145,31 +145,36 @@ uint16_t motor_pins[4][4] = {
     {GPIO_Pin_12, GPIO_Pin_13, GPIO_Pin_14, GPIO_Pin_15} // 모터3
 };
 
+// 압력센서 트리거
+int enter_trigger = 0;
+int out_trigger = 0;
+
 //============================ 함수 프로토타입 선언 ============================
 void RCC_Configure(void);
 void GPIO_Configure(void);
+void TIM2_Configure(void);
 void ADC_Configure(void);
-void USART_Configure(void);
 void NVIC_Configure(void);
 void EXTI_Configure(void);
+void USART1_Init(void);
 
-void set_led_color(uint8_t led_num, uint8_t color);
-void update_leds_based_on_car_presence(void);
+void LED_SetColor(uint8_t led_num, uint8_t color);
+void LED_UpdateByCarPresence(void);
+void Motor_SetSteps(int motor_index, int rotation, int direction);
 
-uint16_t read_adc_value(uint8_t channel);
-
-void step_motor_init(void);
-void set_rpm(int motor_index, int rpm, int direction);
-
-float measure_distance(uint8_t sensor_index);
-void trigger_ultrasonic(uint8_t sensor_index);
+float Ultrasonic_MeasureDistance(uint8_t sensor_index);
+void Ultrasonic_Trigger(uint8_t sensor_index);
 
 void Bluetooth_SendString(char *str);
 
 void EXTI0_IRQHandler(void);
+void EXTI1_IRQHandler(void);
 void USART1_IRQHandler(void);
 
+void delay(int);
+
 //============================ 함수 구현부 ============================
+
 
 void RCC_Configure(void) {
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
@@ -180,6 +185,7 @@ void RCC_Configure(void) {
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);  // TIM1 클럭 활성화
 
     // 타이머 등 필요시 추가
 }
@@ -243,19 +249,20 @@ void GPIO_Configure(void) {
     }
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
     GPIO_Init(ULTRASONIC_ECHO_PORT, &GPIO_InitStructure);
-
-    // USART1 TX: PA9 (AF_PP), USART1 RX: PA10 (IN_FLOATING)
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9; 
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
-    
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
 }
 
+    void TIM2_Configure(void) {
+      TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+
+      // TIM1 설정 (1μs 단위로 동작하도록 설정)
+      TIM_TimeBaseStructure.TIM_Prescaler = 72 - 1;       // 1MHz로 작동 (72MHz / 72)
+      TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up; // 카운터 증가 모드
+      TIM_TimeBaseStructure.TIM_Period = 0xFFFF;          // 최대 카운터 값 (65535)
+      TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1; // 기본 클럭 분주 없음
+      TIM_TimeBaseInit(TIM1, &TIM_TimeBaseStructure);
+
+      TIM_Cmd(TIM1, ENABLE); // TIM1 활성화
+    }
 void USART1_Init(void)
 {
     USART_InitTypeDef USART1_InitStructure;
@@ -317,68 +324,19 @@ void EXTI_Configure(void)
 {
     EXTI_InitTypeDef EXTI_InitStructure;
 
-    // 공통 설정
     EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
-    // Echo 신호는 Rising 엣지에서 타이머 시작, Falling 엣지에서 타이머 정지를 위해 
-    // Rising, Falling 모두 감지할 수 있도록 EXTI_Trigger_Rising_Falling 사용 가능.
-    // 필요에 따라 EXTI_Trigger_Rising, EXTI_Trigger_Falling를 선택하세요.
-    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising_Falling;
+    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling; // 예: 압력센서 신호가 LOW로 떨어질 때 감지
     EXTI_InitStructure.EXTI_LineCmd = ENABLE;
 
-    // Sensor 1: PD3 -> EXTI_Line3
-    GPIO_EXTILineConfig(GPIO_PortSourceGPIOD, GPIO_PinSource3);
-    EXTI_InitStructure.EXTI_Line = EXTI_Line3;
+    // 압력센서 0 : PA0 -> EXTI_Line0
+    GPIO_EXTILineConfig(GPIO_PortSourceGPIOA, GPIO_PinSource0);
+    EXTI_InitStructure.EXTI_Line = EXTI_Line0;
     EXTI_Init(&EXTI_InitStructure);
 
-    // Sensor 2: PD4 -> EXTI_Line4
-    GPIO_EXTILineConfig(GPIO_PortSourceGPIOD, GPIO_PinSource4);
-    EXTI_InitStructure.EXTI_Line = EXTI_Line4;
-    EXTI_Init(&EXTI_InitStructure);
-
-    // Sensor 3: PD7 -> EXTI_Line7
-    GPIO_EXTILineConfig(GPIO_PortSourceGPIOD, GPIO_PinSource7);
-    EXTI_InitStructure.EXTI_Line = EXTI_Line7;
-    EXTI_Init(&EXTI_InitStructure);
-
-    // Sensor 4: PD8 -> EXTI_Line8
-    GPIO_EXTILineConfig(GPIO_PortSourceGPIOD, GPIO_PinSource8);
-    EXTI_InitStructure.EXTI_Line = EXTI_Line8;
-    EXTI_Init(&EXTI_InitStructure);
-
-    // Sensor 5: PD9 -> EXTI_Line9
-    GPIO_EXTILineConfig(GPIO_PortSourceGPIOD, GPIO_PinSource9);
-    EXTI_InitStructure.EXTI_Line = EXTI_Line9;
-    EXTI_Init(&EXTI_InitStructure);
-
-    // Sensor 6: PD10 -> EXTI_Line10
-    GPIO_EXTILineConfig(GPIO_PortSourceGPIOD, GPIO_PinSource10);
-    EXTI_InitStructure.EXTI_Line = EXTI_Line10;
-    EXTI_Init(&EXTI_InitStructure);
-
-    // Sensor 7: PD11 -> EXTI_Line11
-    GPIO_EXTILineConfig(GPIO_PortSourceGPIOD, GPIO_PinSource11);
-    EXTI_InitStructure.EXTI_Line = EXTI_Line11;
-    EXTI_Init(&EXTI_InitStructure);
-
-    // Sensor 8: PD12 -> EXTI_Line12
-    GPIO_EXTILineConfig(GPIO_PortSourceGPIOD, GPIO_PinSource12);
-    EXTI_InitStructure.EXTI_Line = EXTI_Line12;
-    EXTI_Init(&EXTI_InitStructure);
-
-    // Sensor 9: PD13 -> EXTI_Line13
-    GPIO_EXTILineConfig(GPIO_PortSourceGPIOD, GPIO_PinSource13);
-    EXTI_InitStructure.EXTI_Line = EXTI_Line13;
-    EXTI_Init(&EXTI_InitStructure);
-
-    // Sensor 10: PD14 -> EXTI_Line14
-    GPIO_EXTILineConfig(GPIO_PortSourceGPIOD, GPIO_PinSource14);
-    EXTI_InitStructure.EXTI_Line = EXTI_Line14;
-    EXTI_Init(&EXTI_InitStructure);
-
-    // Sensor 11: PD15 -> EXTI_Line15
-    GPIO_EXTILineConfig(GPIO_PortSourceGPIOD, GPIO_PinSource15);
-    EXTI_InitStructure.EXTI_Line = EXTI_Line15;
-    EXTI_Init(&EXTI_InitStructure);
+    // 압력센서 1 : PA1 -> EXTI_Line1
+    GPIO_EXTILineConfig(GPIO_PortSourceGPIOA, GPIO_PinSource1);
+    EXTI_InitStructure.EXTI_Line = EXTI_Line1;
+    EXTI_Init(&EXTI_InitStructure);EXTI0_IRQHandler;
 }
 
 void NVIC_Configure(void) {
@@ -390,7 +348,7 @@ void NVIC_Configure(void) {
     NVIC_EnableIRQ(USART1_IRQn);
     NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x00; 
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x00; 
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x00;
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
     NVIC_Init(&NVIC_InitStructure);
 
@@ -401,22 +359,9 @@ void NVIC_Configure(void) {
     NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x00; 
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
     NVIC_Init(&NVIC_InitStructure);
-
-    // 필요시 EXTI 인터럽트 추가
-}
-void TIM1_Configure(void) {
-    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
-
-    TIM_TimeBaseStructure.TIM_Prescaler = 72 - 1; // 1MHz (1μs 단위)
-    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
-    TIM_TimeBaseStructure.TIM_Period = 0xFFFF; // 최대 카운터 값
-    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
-    TIM_TimeBaseInit(TIM1, &TIM_TimeBaseStructure);
-
-    TIM_Cmd(TIM1, ENABLE); // 타이머 활성화
 }
 
-void set_led_color(uint8_t led_num, uint8_t color) {
+void LED_SetColor(uint8_t led_num, uint8_t color) {
     // led_num: 0,1,2 / color: 0=Green,1=Yellow,2=Red
     switch (led_num) {
         case 0:
@@ -447,43 +392,14 @@ void set_led_color(uint8_t led_num, uint8_t color) {
     GPIO_SetBits(LED_PORT, pin);
 }
 
-// 각 열(column)별 상태:
-// - 모든 칸이 차 있으면(Red)
-// - 모든 칸이 비어있으면(Green)
-// - 그 외(Yellow)
-void update_leds_based_on_car_presence(void) {
-    for (int col = 0; col < 3; col++) {
-        int count = 0;
-        for (int row = 0; row < 3; row++) {
-            if (car_presence[row][col] == 1) count++;
-        }
-        if (count == 3) {
-            set_led_color(col, LED_COLOR_RED); // Red
-        } else if (count == 0) {
-            set_led_color(col, LED_COLOR_GREEN); // Green
-        } else {
-            set_led_color(col, LED_COLOR_YELLOW); // Yellow
-        }
-    }
-}
-//준식이
-uint16_t read_adc_value(uint8_t channel) {
-    ADC_RegularChannelConfig(ADC1, channel, 1, ADC_SampleTime_28Cycles5);
-    ADC_SoftwareStartConvCmd(ADC1, ENABLE);
-    while(!ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC));
-    return ADC_GetConversionValue(ADC1);
-}
-
-void step_motor_init(void) {
-    // 필요시 추가 초기화
-}
-
-void set_rpm(int motor_index, int rpm, int direction) {
+void Motor_SetSteps(int motor_index, int rotation, int direction) {
     uint32_t microseconds_per_minute = 60000000;
     uint32_t total_steps = 4096;  // 1회전당 스텝 수
+    uint32_t total_rotation = total_steps * rotation; // 총회전 수
+    uint32_t rpm = 18; // 기본 RPM 설정
     uint32_t idle_time = microseconds_per_minute / (total_steps * rpm);
 
-    for (uint32_t step = 0; step < total_steps; step++) {
+    for (uint32_t step = 0; step < total_rotation; step++) {
         int sequence_index = (direction == -1) ? (7 - (step % 8)) : (step % 8);
         // 단계에 맞게 핀 설정
         for (int pin = 0; pin < 4; pin++) {
@@ -497,29 +413,38 @@ void set_rpm(int motor_index, int rpm, int direction) {
     }
 }
 
-static void Ultrasonic_Trigger(uint8_t index) {
-    // 해당 센서의 Trigger 핀을 약 10µs 동안 HIGH 유지 후 LOW
-    GPIO_SetBits(ULTRASONIC_TRIG_PORT, ultrasonic_trig_pins[index]);
-    delay_us(10);
-    GPIO_ResetBits(ULTRASONIC_TRIG_PORT, ultrasonic_trig_pins[index]);
+
+void Ultrasonic_Trigger(uint8_t sensor_index) {
+    GPIO_SetBits(ULTRASONIC_TRIG_PORT, ultrasonic_trig_pins[sensor_index]);
+    for(volatile int i=0; i<720; i++); // 약 10us 가정
+    GPIO_ResetBits(ULTRASONIC_TRIG_PORT, ultrasonic_trig_pins[sensor_index]);
+}
+void Trig(uint8_t sensor_index) {
+    GPIO_SetBits(GPIOC, ultrasonic_trig_pins[sensor_index]);  // Trig 핀 HIGH
+    delay_us(10);                      // 10μs 유지
+    GPIO_ResetBits(GPIOC, ultrasonic_trig_pins[sensor_index]); // Trig 핀 LOW
 }
 
-float measure_distance(uint8_t sensor_index) {
+float Ultrasonic_MeasureDistance(uint8_t sensor_index) {
     // 초음파 거리 측정 로직 필요
     // 여기서는 틀만 제공
-      uint16_t start_time = 0, stop_time = 0, echo_time = 0;
+  uint16_t start_time = 0, stop_time = 0, echo_time = 0;
     float distance = 0.0;
-    // Trig 신호 송출
-    Ultrasonic_Trigger(sensor_index);
-    // Echo 핀 HIGH 대기
-    while (GPIO_ReadInputDataBit(ULTRASONIC_ECHO_PORT, ultrasonic_echo_pins[sensor_index]) == 0);
+
+    Trig(sensor_index); // Trig 신호 송출
+
+    // Echo 핀 HIGH 상태 대기
+    while (GPIO_ReadInputDataBit(GPIOD, ultrasonic_echo_pins[sensor_index]) == 0);
+
     // Echo 핀 HIGH 시작 시간 기록
     start_time = TIM_GetCounter(TIM1);
-    // Echo 핀이 LOW로 전환될 때까지 대기
-    while (GPIO_ReadInputDataBit(ULTRASONIC_ECHO_PORT, ultrasonic_echo_pins[sensor_index]) == 1);
+
+    // Echo 핀이 LOW 상태로 전환될 때까지 대기
+    while (GPIO_ReadInputDataBit(GPIOD, ultrasonic_echo_pins[sensor_index]) == 1);
+
     // Echo 핀 HIGH 종료 시간 기록
     stop_time = TIM_GetCounter(TIM1);
-    printf("%u", stop_time);
+
     // Echo 핀의 HIGH 지속 시간 계산
     if (stop_time >= start_time) {
         echo_time = stop_time - start_time;
@@ -528,9 +453,8 @@ float measure_distance(uint8_t sensor_index) {
     }
 
     // 거리 계산 (단위: cm)
-    distance = (float)(echo_time * 0.0343) / 2.0;
+    distance = (float)(echo_time * 0.0343) / 2.0; // 속도: 343m/s
 
-    // Echo 측정 로직 필요
     return distance;
 }
 
@@ -555,103 +479,6 @@ void map_sensor_to_rc(int sensor, int *r, int *c) {
         default:*r=-1;*c=-1;break;
     }
 }
-
-// 초음파 센서 인터럽트 핸들러 예제 (센서 Echo 핀 Rising/Falling 엣지 감지)
-// 실제로는 Echo 신호의 Rising 시각 기록, Falling시각 기록 후 거리 계산 필요
-// 여기서는 단순히 인터럽트가 발생하면 distance를 측정했다고 가정하고
-// 그 결과에 따라 car_presence 설정 예제 코드만 제시
-void EXTI1_IRQHandler(void) { // 예: 센서1 Echo 핸들러
-    if (EXTI_GetITStatus(EXTI_Line1) != RESET) {
-        int r,c;
-        float dist = measure_distance(1);
-        map_sensor_to_rc(1,&r,&c);
-        if (r>=0 && c>=0) {
-            if (dist < 20.0f && dist > 0.0f) car_presence[r][c] = 1;
-            else car_presence[r][c] = 0;
-        }
-        EXTI_ClearITPendingBit(EXTI_Line1);
-    }
-}
-
-// 이거 EXTI 
-void EXTI4_IRQHandler(void) {
-    if (EXTI_GetITStatus(EXTI_Line4) != RESET) {
-        float dist = measure_distance(4);
-        int r, c;
-        map_sensor_to_rc(7, &r, &c);
-        if (r >= 0 && c >= 0) {
-            if (dist < DISTANCE_THRESHOLD && dist > 0.0f) car_presence[r][c] = 1;
-            else car_presence[r][c] = 0;
-        }
-        EXTI_ClearITPendingBit(EXTI_Line4);
-    }
-}
-
-void EXTI7_IRQHandler(void) {
-    if (EXTI_GetITStatus(EXTI_Line7) != RESET) {
-        float dist = measure_distance(7);
-        int r, c;
-        map_sensor_to_rc(7, &r, &c);
-        if (r >= 0 && c >= 0) {
-            if (dist < DISTANCE_THRESHOLD && dist > 0.0f) car_presence[r][c] = 1;
-            else car_presence[r][c] = 0;
-        }
-        EXTI_ClearITPendingBit(EXTI_Line7);
-    }
-}
-
-void EXTI8_IRQHandler(void) {
-    if (EXTI_GetITStatus(EXTI_Line8) != RESET) {
-        float dist = measure_distance(8);
-        int r, c;
-        map_sensor_to_rc(8, &r, &c);
-        if (r >= 0 && c >= 0) {
-            if (dist < DISTANCE_THRESHOLD && dist > 0.0f) car_presence[r][c] = 1;
-            else car_presence[r][c] = 0;
-        }
-        EXTI_ClearITPendingBit(EXTI_Line8);
-    }
-}
-
-void EXTI9_IRQHandler(void) {
-    if (EXTI_GetITStatus(EXTI_Line9) != RESET) {
-        float dist = measure_distance(9);
-        int r, c;
-        map_sensor_to_rc(9, &r, &c);
-        if (r >= 0 && c >= 0) {
-            if (dist < DISTANCE_THRESHOLD && dist > 0.0f) car_presence[r][c] = 1;
-            else car_presence[r][c] = 0;
-        }
-        EXTI_ClearITPendingBit(EXTI_Line9);
-    }
-}
-
-void EXTI10_IRQHandler(void) {
-    if (EXTI_GetITStatus(EXTI_Line10) != RESET) {
-        float dist = measure_distance(10);
-        int r, c;
-        map_sensor_to_rc(10, &r, &c);
-        if (r >= 0 && c >= 0) {
-            if (dist < DISTANCE_THRESHOLD && dist > 0.0f) car_presence[r][c] = 1;
-            else car_presence[r][c] = 0;
-        }
-        EXTI_ClearITPendingBit(EXTI_Line10);
-    }
-}
-
-void EXTI11_IRQHandler(void) {
-    if (EXTI_GetITStatus(EXTI_Line11) != RESET) {
-        float dist = measure_distance(11);
-        int r, c;
-        map_sensor_to_rc(11, &r, &c);
-        if (r >= 0 && c >= 0) {
-            if (dist < DISTANCE_THRESHOLD && dist > 0.0f) car_presence[r][c] = 1;
-            else car_presence[r][c] = 0;
-        }
-        EXTI_ClearITPendingBit(EXTI_Line11);
-    }
-}
-
 
 
 // 이런 식으로 3,4,7,8,9,10,11에 대한 EXTI 핸들러도 동일한 패턴으로 구현
@@ -692,19 +519,20 @@ void USART2_IRQHandler() {
     }
 }
 
+// 압력센서 인터럽트 핸들러 만들기
 
-void update_leds_based_on_car_presence(void) {
+void LED_UpdateByCarPresence(void) {
     for (int col = 0; col < 3; col++) {
         int count = 0;
         for (int row = 0; row < 3; row++) {
             if (car_presence[row][col] == 1) count++;
         }
         if (count == 2) {
-            set_led_color(col, LED_COLOR_RED); // Red
+            LED_SetColor(col, LED_COLOR_RED); // Red
         } else if (count == 0) {
-            set_led_color(col, LED_COLOR_GREEN); // Green
+            LED_SetColor(col, LED_COLOR_GREEN); // Green
         } else {
-            set_led_color(col, LED_COLOR_YELLOW); // Yellow
+            LED_SetColor(col, LED_COLOR_YELLOW); // Yellow
         }
     }
 }
@@ -713,75 +541,81 @@ void delay(int step){
     for (volatile int i = 0; i < step; i++);
 }
 
+void EXTI0_IRQHandler(void) {
+    if (EXTI_GetITStatus(EXTI_Line0) != RESET) {
+        // 입구 압력센서 감지
+        enter_trigger = 1;
+        EXTI_ClearITPendingBit(EXTI_Line0);
+    }
+}
+
+void EXTI1_IRQHandler(void) {
+    if (EXTI_GetITStatus(EXTI_Line1) != RESET) {
+        // 출구 압력센서 감지
+        out_trigger = 1;
+        EXTI_ClearITPendingBit(EXTI_Line1);
+    }
+}
+
 //============================ 메인 함수 ============================
 int main(void) {
+    float distance;
+  
     SystemInit();
     RCC_Configure();
     GPIO_Configure();
     ADC_Configure();
-    USART_Configure();
+    TIM2_Configure();      // 타이머 초기화 (준식)
     USART1_Init(); // PC
     USART2_Init(); // 블루투스
     NVIC_Configure();
-    step_motor_init();
-
+    
     // 초기 LED 상태 (모두 Red)
-    set_led_color(0, 2);
-    set_led_color(1, 2);
-    set_led_color(2, 2);
-
+    LED_SetColor(0, 2);
+    LED_SetColor(1, 2);
+    LED_SetColor(2, 2);
+    
     while(1) {
         // 각 초음파 센서(1~9)로 거리 측정하고 일정 거리 이하면 차 있음(1), 아니면 없음(0)
         // sensor_index: 1,2,3  / 4,5,6 / 7,8,9 => 3x3
         // row = (sensor_index-1)/3, col = (sensor_index-1)%3
+        distance = Ultrasonic_MeasureDistance(0);
+        printf("%f\n",distance);
+        if (enter_trigger){
+            // 문 개방
 
-        for (int s = 1; s <= 3; s++) {
-            float dist = measure_distance(s);
-            // 예: dist < 20cm 이면 차 있다고 가정
-            if (dist > 0 && dist < DISTANCE_THRESHOLD) {
-                car_presence[(s-1)/3][(s-1)%3] = 1;
-                
-            } else {
-                car_presence[(s-1)/3][(s-1)%3] = 0;
-            }
-        }
-        for(int i = 0;i<3;i++)
-        {
-            for(int j = 1;j<=3;j++)
+            // 초음파 센서 9개 전부 트리거 발생 시작
+            for(int i = 0; i<ULTRASONIC_COUNT; i++)
             {
-                printf("%d ",car_presence[(i*3) + j]);
-                if(j == 3)
-                {
-                    printf("\n"); 
-                }
+              distance = Ultrasonic_MeasureDistance(i);  
             }
-        }
-        printf("\n");
-        // 차 유무에 따라 LED 업데이트
-        update_leds_based_on_car_presence();
+            
+            // 만약 초음파 센서 중 하나에서 차가 감지가 되면 car_presence 값 변경
+            
+            // 차 유무에 따라 LED 업데이트
+            LED_UpdateByCarPresence();
 
-        // 입구 초음파(0)로 차량 감지
-        float dist_entrance = measure_distance(0);
-        if (dist_entrance < 10.0f && dist_entrance > 0.0f) {
-            // 차량 입구 도착 감지 -> 문 개방 (모터0 예)
-            set_rpm(0, 10);
+           
         }
-
-        // 압력센서 확인 (0~3)
-        uint16_t adc_val0 = read_adc_value(ADC_Channel_0); // 압력센서0
-        if (adc_val0 > 500) {
-            // 수직 이동 모터 동작 (예: 모터1)
-            set_rpm(1, 13); 
+        if (out_trigger){
+            // 사람이 출구를 나가는 것이 감지가 되는 경우
+            
+            // 모터 수직 이동, 방금 들어온 차를 보고 모터 index와 방향을 결정해야함
         }
-
         // 블루투스 출차 명령
         if (bluetooth_command_received) {
             bluetooth_command_received = 0;
             if (strcmp((char*)bluetooth_rx_buffer, "OUT") == 0) {
+                // 일단 테스트를 위해 차량 하강
+                Motor_SetSteps(1, 13, -1); // direction = -1 (역방향)
+
+                // 실제 구현에서는 주차 공간을 토대로 판단해서 방향을 결정해야함.
                 // 차량 하강 (예: 모터1 반대방향 구현 필요)
-                set_rpm(1, 13, -1); // direction = -1 (역방향)
+                Motor_SetSteps(1, 13, -1); // direction = -1 (역방향)
             }
         }
         delay(1000000);
     }
+
+    Motor_SetSteps(1, 3, 1);
 }
